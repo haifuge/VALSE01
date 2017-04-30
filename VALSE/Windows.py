1f0300258c96eb428ebebfe8c13a66c7c65ce06a
haifuge <ge.haifu@gmail.com>
2017-04-30 10:29:08 -0400

    add window codes

---
+++ VALSE/Windows.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import TimeLine
+
+class MainWindow(QMainWindow):
+    count=0
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('VALSE')
+        self.mdi=QMdiArea()
+        self.setCentralWidget(self.mdi)
+        bar=self.menuBar()
+        file=bar.addMenu("File")
+        file.addAction("New")
+        file.addAction("Cascade")
+        file.addAction("Tiled")
+        file.triggered[QAction].connect(self.windowAction)
+    def windowAction(self, q):
+        print("triggered")
+        if q.text()=="New":
+            self.count=self.count+1
+            sub=QMdiSubWindow()
+            sub.setWidget(TimeLine.TimeLine())
+            sub.setWindowTitle("subwindow"+str(self.count))
+            self.mdi.addSubWindow(sub)
+            sub.show()
+        if q.text()=='Cascade':
+            self.mdi.cascadeSubWindows()
+        if q.text()=='Titled':
+            self.mdi.tileSubWindows()
+
+if __name__=='__main__':
+    app=QApplication(sys.argv)
+    ex=MainWindow()
+    ex.show()
+    sys.exit(app.exec_())
