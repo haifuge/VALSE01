a1a439179b98e6802cf9365227861c001921f2c4
haifuge <ge.haifu@gmail.com>
2017-06-05 21:42:15 -0400

    update sidebar item and add visible icon

--- VALSE/UI/Sidebar/PeopleItem.py
+++ VALSE/UI/Sidebar/PeopleItem.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import *
 
 
 class PeopleItem(QWidget):
+    visible=True;
     def __init__(self):
         super().__init__();
         self.initUI()
@@ -21,9 +22,29 @@ class PeopleItem(QWidget):
         self.txtName.setReadOnly(True)
         self.txtName.setFrame(False)
         bgColor=self.palette().color(QPalette.Background)
-        print(bgColor)
         self.txtName.setStyleSheet('background-color: ' + bgColor.name())
 
+        self.btnMarker=MarkerButton(self)
+        self.btnMarker.setGeometry(QRect(160,2,20,20))
+        self.btnMarker.clicked.connect(self.markerClicked)
+        self.btnMarker.setStyleSheet('background-color: ' + bgColor.name()+'; border:none;')
+
+        self.btnVisible=QPushButton(self)
+        self.btnVisible.setIcon(QIcon(r'Pictures/visible.png'))
+        self.btnVisible.clicked.connect(self.visibleClicked)
+        self.btnVisible.setGeometry(190,2,20,20)
+        self.btnVisible.setStyleSheet('background-color: ' + bgColor.name()+'; border:none;')
+        self.visible=True
+
+    def markerClicked(self):
+        print('clicked')
+    def visibleClicked(self):
+        if self.visible:
+            self.visible=False
+            self.btnVisible.setIcon(QIcon(r'Pictures/invisible.png'))
+        else:
+            self.visible=True
+            self.btnVisible.setIcon(QIcon(r'Pictures/visible.png'))
 class QtText(QLineEdit):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -31,7 +52,6 @@ class QtText(QLineEdit):
     def focusOutEvent(self, e):
         self.setReadOnly(True)
     def mouseDoubleClickEvent(self, e):
-        print('double clicked')
         if self.isReadOnly():
             self.setReadOnly(False)
         else:
@@ -41,6 +61,20 @@ class QtText(QLineEdit):
             self.setReadOnly(True)
         else:
             super().keyPressEvent(e)
+
+
+class MarkerButton(QPushButton):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.resize(20,20)
+    def paintEvent(self,e):
+        qp=QPainter()
+        qp.begin(self)
+        self.drawMarker(qp)
+        qp.end()
+    def drawMarker(self, qp):
+        qp.setPen(Qt.red)
+        qp.drawRect(1,1,18,18)
 
 if __name__=='__main__':
     app=QApplication(sys.argv)
